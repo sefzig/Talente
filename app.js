7f6213d4089206c9ae63a4818580327ab5727e62
Andreas Sefzig <andreas@sefzig.net>
2016-06-16 15:24:17 +0200

    View

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 
-app.use("/img", express.static(__dirname + '/views/img'));
-app.use("/css", express.static(__dirname + '/views/css'));
-app.use("/js", express.static(__dirname + '/views/js'));
-app.use("/client", express.static(__dirname + '/views/client')); // live: '/views/client/_talente' // lokal: '/views/client/'
-//app.use("/client/img", express.static(__dirname + '/views/client/_talente/img')); // live: '/views/client/_talente' // lokal: '/views/client/'
+app.use('/img', express.static(__dirname + '/views/img'));
+app.use('/css', express.static(__dirname + '/views/css'));
+app.use('/js', express.static(__dirname + '/views/js'));
+app.use('/client', express.static(__dirname + '/views/client/')); // live: '/views/client/_chatraum' // lokal: '/views/client/'
+app.use('/client/img', express.static(__dirname + '/views/client/_chatraum/img')); // live: '/views/client/_chatraum' // lokal: '/views/client/'
 
 app.get('/', function(req, res) {
     res.render('index', {
