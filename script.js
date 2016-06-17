bcfde32006b21db7da32a245bd65a57e28556cf1
Andreas Sefzig <andreas@sefzig.net>
2016-06-17 14:55:13 +0200

    Skript

--- script.js
+++ script.js
@@ -92,40 +92,6 @@
         }
         
     },
-
-    erklart: {
-    	
-        prompt: (bot) => bot.say(EmpfangsBot+'Dieser Chat hat ein Menü, in dem Sie alle wichtigen Inhalte finden!')
-             .then(() => bot.say(EmpfangsBot+'Ich habe es für Sie geöffnet. [Javascript:menu()] Sie können es öffnen und schließen, indem Sie --Menü schreiben. Bitte schauen Sie sich das Menü kurz an und schließen Sie es.')),
-        
-        receive: (bot, message) => {
-            
-            var menu = message.text;
-            var menu_gross = befehlWort(message.text.trim().toUpperCase());
-            var beantwortet = false;
-            	
-            if (~menu_gross.indexOf("--MENÜ")) { 
-            
-               beantwortet = true;
-               return bot.setProp('menuverstanden', 'ja')
-               .then(() => bot.say(Sie haben das Menü verstanden.'))
-               .then(() => bot.say(EmpfangsBot+'Ihr Ansprechpartner: Frau '+prop_ansprechpartner+'. Ihre E-Mail-Adresse: '+prop_emailadresse+'. Ihre Telefon-Nummer: '+prop_telefonnummer+'. Weiter zum --Empfang?'))
-               .then(() => 'empfang');
-               
-            }
-            
-            if (beantwortet == false) { 
-            
-               return bot.setProp('menuverstanden', 'nein')
-                  .then(() => bot.say(EmpfangsBot+'Sie können das Menü auch mit dem Button rechts oben bedienen.'))
-                  .then(() => bot.say(EmpfangsBot+'Ihr Ansprechpartner: Frau '+prop_ansprechpartner+'. Ihre E-Mail-Adresse: '+prop_emailadresse+'. Ihre Telefon-Nummer: '+prop_telefonnummer+'. Weiter zum --Empfang?'))
-                  .then(() => 'empfang');
-            
-            }
-            
-        }
-        
-    },
    
     ansprechpartner: {
     	
